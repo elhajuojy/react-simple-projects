e1de918f4f32ba3bb28c0a30aecb8806a4833630
elhajuojy <souirimehdi311@gmail.com>
2022-07-20 23:24:08 +0100

    finsh Aribnb design

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from "react";
-
-
+import Nav from "./Nav";
+import Hero from "./Hero";
+import Main from './Main'
 
 
 
 function App() {
     return (
         <div>
+            <Nav />
+            <Hero />
+            <Main />
+
 
         </div>
 
---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import hero_image from '../images/Group 77.png'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <img src={hero_image} alt='hero_image' className='hero--img' />
+
+
+        </div>
+    );
+};
+
+export default Hero;
