4e7a98fbe8022333f23fc11979e80f20046a4ae6
elhajuojy <souirimehdi311@gmail.com>
2022-07-24 11:45:15 +0100

    finsh the start Quiz Screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Start from './components/Start';
+
 
 const App = () => {
     return (
         <div>
-            <h1>Hello World</h1>
+            <Start />
 
         </div>
     );
---
+++ src/components/Start.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Start = () => {
+    return (
+        <div className='start container'>
+            <h1>Quizzical</h1>
+            <p>Some description if needed</p>
+            <button className='btn-primary'>Start quiz</button>
+
+        </div>
+    );
+};
+
+export default Start;
